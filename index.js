31a55ed7ee5a3a47220e4714842e8a1aa5476ce1
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-05-11 16:23:04 -0500

    Envio dos produtos para o front end usando o get, incluindo envio do _id do mongo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express, {json} from "express";
 import dotenv from "dotenv";
-import {MongoClient} from "mongodb"
+import chalk from "chalk";
+import { MongoClient, ObjectId } from "mongodb";
 import joi from "joi";
 import cors from "cors";
 
@@ -8,6 +9,10 @@ const app = express();
 app.use(json());
 app.use(cors());
 dotenv.config();
+
+/* url do mongo para deploy
+MONGO_URL=mongodb+srv://projetao:ljwIFCV7qVYWxuTU@livrariat5.d682m.mongodb.net/projetao14?retryWrites=true&w=majority
+*/
 
 let db = null;
 const mongoClient = new MongoClient(process.env.MONGO_URL)
@@ -23,7 +28,35 @@ try {
 
 //aqui vai ser o post
 
-// Implementar feature de produtos
+const livros = [
+  {titulo: "titulo1", imagem: "imagem1", preco: "preço1", id: 1},
+  {titulo: "titulo2", imagem: "imagem2", preco: "preço2", id: 2},
+  {titulo: "titulo3", imagem: "imagem3", preco: "preço3", id: 3},
+  {titulo: "titulo4", imagem: "imagem4", preco: "preço4", id: 4},
+  {titulo: "titulo5", imagem: "imagem5", preco: "preço5", id: 5},
+];
+
+// Comandos usados para criar o banco de dados de livros
+
+  // try {
+  //   livros.forEach(livro => {
+  //     db.collection("livros").insertOne(livro);
+  //     console.log("Salvou o livro no banco");
+  //   })
+  //   res.send("Livros salvos no banco").status(201);
+  // }
+
+app.get("/products", async (req, res) => {
+  try {
+    const produtos = await db.collection("livros").find().toArray();
+    console.log(chalk.bold.green(produtos));
+    res.send(produtos).status(201);
+  }
+  catch (error) {
+    console.log("Erro ao conectar MongoDB");
+    console.log("erro",error);
+  }
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
