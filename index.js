04ace7b1c55657e37fcdc06214cee9a0875bf628
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-05-13 17:33:46 -0500

    Validaçao do token feita na pagina de checkout

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 import chalk from "chalk";
 import { MongoClient, ObjectId } from "mongodb";
+import { v4 as uuid } from 'uuid';
 import joi from "joi";
 import cors from "cors";
 import bcrypt from "bcrypt";
@@ -16,16 +17,16 @@ const mongoClient = new MongoClient(process.env.MONGO_URL)
 
 try {
   await mongoClient.connect();
-    db = mongoClient.db(process.env.DATABASE);
-    console.log("Sucesso ao conectar MongoDB")
+  db = mongoClient.db(process.env.DATABASE);
+  console.log("Sucesso ao conectar MongoDB")
 } catch (error) {
-    console.log("Erro ao conectar MongoDB");
-    console.log("erro",error);
+  console.log("Erro ao conectar MongoDB");
+  console.log("erro", error);
 }
 
 app.post("/cadastrar", async (req, res) => {
-  
-  const {name, email, password, confirmPassword} = req.body;
+
+  const { name, email, password, confirmPassword } = req.body;
 
   const cadastrarSchema = joi.object({
     name: joi.string().required(),
@@ -39,35 +40,36 @@ app.post("/cadastrar", async (req, res) => {
       name,
       email,
       password,
-      confirmPassword}, {abortEarly: false});
-      
-  } catch (error){
+      confirmPassword
+    }, { abortEarly: false });
+
+  } catch (error) {
     res.status(422).send("deu erro");
     //return;
   }
 
   try {
-    const cliente = await db.collection("clientes").findOne({email})
-    if (cliente){
-        console.log("ja cadastrado")
-        res.sendStatus(400);
-        return;
+    const cliente = await db.collection("clientes").findOne({ email })
+    if (cliente) {
+      console.log("ja cadastrado")
+      res.sendStatus(400);
+      return;
     }
   } catch (error) {
-    
+
   }
 
   try {
     const SALT = 10;
     const passwordHash = bcrypt.hashSync(password, SALT);
-    
+
     await db.collection("clientes").insertOne({
       name,
       email,
       password: passwordHash
     });
     return res.sendStatus(201);
-    
+
   } catch (error) {
     console.log("Erro ao criar cliente");
     console.log(error);
@@ -76,8 +78,8 @@ app.post("/cadastrar", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  
-  const {email, password} = req.body;
+
+  const { email, password } = req.body;
 
   const loginSchema = joi.object({
     email: joi.string().email().required(),
@@ -86,27 +88,27 @@ app.post("/login", async (req, res) => {
 
   try {
     await loginSchema.validateAsync(
-    {
-      email,
-      password
-    }, {abortEarly: false});
-      
-  } catch (error){
+      {
+        email,
+        password
+      }, { abortEarly: false });
+
+  } catch (error) {
     res.status(422).send("deu erro");
     //return;
   }
 
   try {
-    const cliente = await db.collection("clientes").findOne({email: email})
-    if(!cliente) return res.sendStatus(404);
-    if(cliente && bcrypt.compareSync(password, cliente.password)) {
+    const cliente = await db.collection("clientes").findOne({ email: email })
+    if (!cliente) return res.sendStatus(404);
+    if (cliente && bcrypt.compareSync(password, cliente.password)) {
       const token = uuid();
-      await db.collection("sessions").insertOne({token, clienteId: cliente._id});
-      return res.send({token, name: cliente.name});
+      await db.collection("sessions").insertOne({ token, clienteId: cliente._id });
+      return res.send({ token, name: cliente.name });
     }
-    return res.sendStatus(404);         
+    return res.sendStatus(404);
 
-  } 
+  }
   catch (error) {
     console.log("Erro, cliente nao encontrado");
     console.log(error);
@@ -115,7 +117,7 @@ app.post("/login", async (req, res) => {
 });
 
 // const livros = [
-//   {title: "Os segredos da mente milionária", author:"T.Harv Eker",  price: "R$39,90", id: 1, 
+//   {title: "Os segredos da mente milionária", author:"T.Harv Eker",  price: "R$39,90", id: 1,
 //   description:`Se as suas finanças andam na corda bamba, talvez esteja na hora de você refletir sobre o que T. Harv Eker chama de "o seu modelo de dinheiro" – um conjunto de crenças que cada um de nós alimenta desde a infância e que molda o nosso destino financeiro, quase sempre nos levando para uma situação difícil. Neste livro, Eker mostra como substituir uma mentalidade destrutiva – que você talvez nem perceba que tem – pelos "arquivos de riqueza", 17 modos de pensar e agir que distinguem os ricos das demais pessoas.`,
 //   image: "",
 // },
@@ -135,25 +137,25 @@ app.get("/products", async (req, res) => {
   }
   catch (error) {
     console.log("Erro ao obter os produtos");
-    console.log("erro",error);
+    console.log("erro", error);
   }
 });
 
 app.get("/products/:id", async (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   try {
-    const procuraProduto = await db.collection("livros").findOne({_id: new ObjectId(id)});
+    const procuraProduto = await db.collection("livros").findOne({ _id: new ObjectId(id) });
     if (!procuraProduto) return res.sendStatus(401);
     else res.send(procuraProduto).status(200);
   }
   catch (error) {
     console.log("Erro ao obter um produto específico");
-    console.log("erro",error);
+    console.log("erro", error);
   }
 })
 
-app.post("/carrinho", async (req,res) => {
-  const {title, author, price} = req.body;
+app.post("/carrinho", async (req, res) => {
+  const { title, author, price } = req.body;
   const livroCarrinho = {
     title,
     author,
@@ -166,7 +168,35 @@ app.post("/carrinho", async (req,res) => {
   }
   catch (error) {
     console.log("Erro ao enviar o produto pro carrinho");
-    console.log("erro",error);
+    console.log("erro", error);
+  }
+});
+
+app.get("/checkout", async (req, res) => {
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer', '').trim();
+  // 1a validação: Verifica se o token é válido
+  if (!token) return res.send("Token inexistente").status(401);
+  else console.log("Passou na primeira validação");
+  try {
+    // 2a validação: Verifica se o token existe na coleção dos tokens
+    const session = await db.collection("sessions").findOne({ token })
+    if (!session) return res.sendStatus(401);
+    else console.log("Passou na segunda validação")
+
+    // 3a validação: Busca os dados do usuário associado ao token na coleção de informações
+    const user = await db.collection("clientes").findOne({ _id: session.clienteId });
+    if (!user) res.sendStatus(404);
+    else {
+      console.log("Passou na terceira validação");
+      return res.send(user).status(200);
+    }
+    // const registro = await db.collection("clientes").find({ name: user.nome }).toArray();
+  }
+
+  catch (error) {
+    console.error(error);
+    res.status(500).send(chalk.red.bold("Falha na verificação do token"))
   }
 });
 
