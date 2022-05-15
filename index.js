f48fb0e03bdd524b8988709140e0958813b8264c
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-05-15 18:21:53 -0300

    criada rota e controller para checkout

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import productsRouter from "./Routes/productsRouters.js"
 import authRouters from "./Routes/authRouters.js";
 import carrinhoRouters from "./Routes/carrinhoRouters.js";
 import adressRouters from "./Routes/adressRouters.js";
+import checkoutRouters from "./Routes/checkoutRouters.js"
 
 const app = express();
 app.use(cors());
@@ -21,34 +22,8 @@ app.use(productsRouter);
 app.use(authRouters)
 app.use(carrinhoRouters)
 app.use(adressRouters)
+app.use(checkoutRouters)
 
-app.get("/checkout", async (req, res) => {
-  const { authorization } = req.headers;
-  const token = authorization?.replace('Bearer', '').trim();
-  // 1a validação: Verifica se o token é válido
-  if (!token) return res.send("Token inexistente").status(401);
-  else console.log("Passou na primeira validação");
-  try {
-    // 2a validação: Verifica se o token existe na coleção dos tokens
-    const session = await db.collection("sessions").findOne({ token })
-    if (!session) return res.sendStatus(401);
-    else console.log("Passou na segunda validação")
-
-    // 3a validação: Busca os dados do usuário associado ao token na coleção de informações
-    const user = await db.collection("clientes").findOne({ _id: session.clienteId });
-    if (!user) res.sendStatus(404);
-    else {
-      console.log("Passou na terceira validação");
-      return res.send(user).status(200);
-    }
-    // const registro = await db.collection("clientes").find({ name: user.nome }).toArray();
-  }
-
-  catch (error) {
-    console.error(error);
-    res.status(500).send(chalk.red.bold("Falha na verificação do token"))
-  }
-});
 
 app.post("/finalizar", async (req, res) => {
   const {authorization} = req.headers;
